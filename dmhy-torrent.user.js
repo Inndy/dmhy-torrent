1710e8f3ca799b35b366b298a60c9554eb848f65
Inndy <inndy.tw@gmail.com>
2017-08-12 08:17:53 +0000

    Change table header and add semi-colon

--- dmhy-torrent.user.js
+++ dmhy-torrent.user.js
@@ -1,4 +1,6 @@
 // ==UserScript==
+// @author      Inndy Lin
+// @description Easy download torrent from share.dmhy.org
 // @name        dmhy-torrent
 // @namespace   DMHY
 // @include     http://share.dmhy.org/*
@@ -9,37 +11,51 @@
 // @grant       none
 // ==/UserScript==
 
-function getTorrentUrl(src) {
+var CHT2CHS = {
+  '種子': '种子'
+};
+
+var LANG_TYPE = navigator.language.match(/zh-CN|zh-SG/) ? 'zh-CN' : 'zh-TW';
+
+function _s(txt)
+{
+  return LANG_TYPE == 'zh-TW' ? txt : (CHT2CHS[txt] || txt);
+}
+
+function getTorrentUrl (src)
+{
   return new Promise(function (resolve, reject) {
     jQuery.ajax({
       url: src,
       dataType: 'html',
       success: function (response) {
-        var parsed = jQuery(response)
-        var node = parsed.find('#resource-tabs > div > p > a:first')
-        var url = node.attr('href')
+        var parsed = jQuery(response);
+        var node = parsed.find('#resource-tabs > div > p > a:first');
+        var url = node.attr('href');
         if (url && url.length) {
-          resolve(url)
+          resolve(url);
         } else {
-          reject()
+          reject();
         }
       }
-    })
-  })
+    });
+  });
 }
 
-function install() {
-  $("#topic_list tbody tr td a.download-arrow").click(function () {
-    var node = $(this).parents('tr').find('td.title > a')
-    var link = node.attr('href')
+function install ()
+{
+  $('#topic_list > thead > tr > th:nth-child(4) > span').text(_s('種子'));
+  $('#topic_list > tbody > tr > td > a.download-arrow').click(function () {
+    var node = $(this).parents('tr').find('td.title > a');
+    var link = node.attr('href');
     getTorrentUrl(link).then(
-      src => { location.href = src },
+      src => { location.href = src; },
       () => $(this).parent().text('X').css('color', 'red')
-    )
-    return false
-  })
+    );
+    return false;
+  });
 }
 
-if (jQuery && jQuery("#topic_list").length == 1) {
-  install()
+if (jQuery && jQuery('#topic_list').length == 1) {
+  install();
 }
